03ba6f306d9acd91c846ee807301fe23d4306aba
Michael C <159742740+miconnors812@users.noreply.github.com>
2025-05-10 12:24:20 -0400

    Added comments to Coordinator.dart
    Similar to my python comments but updated to fit dart

--- lib/models/Coordinator.dart
+++ lib/models/Coordinator.dart
@@ -15,6 +15,9 @@ class Coordinator extends User {
     //this.isCoordinator = true;
   }
 
+  '''
+  Initializes the User as a Coordinator of a certain club that will be set later by setting isCoordinator to true
+  '''
   void setCoordinator() {
     if (!isCoordinator) {
       isCoordinator = true;
@@ -23,6 +26,12 @@ class Coordinator extends User {
     }
   }
 
+  '''
+  This function assigns a club for the Coordinator to have permission to coordinate 
+
+  Parameter(s): 
+  club: A Club object to determine the club that the Coordinator can access and manage
+  '''
   void setCoordinatedClub(Club club) {
     if (!club.isAdmin(this.userId)) {
       club.addAdmin(this.userId);
@@ -36,6 +45,13 @@ class Coordinator extends User {
     }
   }
 
+  '''
+  Adds an event to a club hosted by the coordinator and to the coordinator's list of events
+
+  Parameter(s): 
+  event: an Event object that will be added to this Coordinator's club 
+  club: the intended club that the event will be associated with
+  '''
   void hostEvent(Event event, Club club) {
     if (club.isAdmin(this.userId)) {
       if (!managedEvents.contains(event) && !club.getEventList().contains(event)) {
@@ -50,7 +66,15 @@ class Coordinator extends User {
       print("Invalid Permissions! You are not an Admin for this club.");
     }
   }
+  
+  '''
+  Ends an event from a club hosted by the coordinator by removing it from the club
+  and the Coordinator's list of managed events
 
+  Parameter(s): 
+  event: an Event object that will be terminated from this Coordinator's club 
+  club: the intended club that the event will be no longer be associated with
+  '''
   void endEvent(Event event, Club club) {
     if (club.isAdmin(this.userId)) {
       int index = coordinatedClubs.indexOf(club);
@@ -67,6 +91,9 @@ class Coordinator extends User {
     }
   }
 
+  '''
+  Demotes the User from their role as a Coordinator of a certain club
+  '''
   void demote() {
     if (isCoordinator) {
       isCoordinator = false;
@@ -80,14 +107,27 @@ class Coordinator extends User {
     }
   }
 
+  '''
+  Getter for the Coordinator's currently managed events
+  '''
   List<Event> getManagedEvents() {
     return managedEvents;
   }
 
+  '''
+  Getter for the clubs that the Coordinator is an admin for
+  '''
   List<Club> getManagedClubs() {
     return coordinatedClubs;
   }
 
+  '''
+  Relinquish admin permissions from the club if it is one that the Coordinator coordinates
+  The Coordinator remains as a normal member of this club, just not an admin.
+
+  Paramaters:
+  club - Club that you no longer want to be in charge of
+  '''
   void relegateAdmin(Club club) {
     if (club.isAdmin(this.userId)) {
       if (coordinatedClubs.contains(club)) {
